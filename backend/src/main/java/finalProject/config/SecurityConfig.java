e506bccbee71ba221c036f795133c3f803bac76c
Arginbekuly <kzantureev@gmail.com>
2026-04-14 05:23:39 +0500

    Update user service URL in configuration files and adjust security request matchers

--- backend/src/main/java/finalProject/config/SecurityConfig.java
+++ backend/src/main/java/finalProject/config/SecurityConfig.java
@@ -35,7 +35,6 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/v1/auth/login",
                                 "/api/v1/auth/register",
-                                "/api/v1/**",
                                 "/error",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
